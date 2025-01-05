4ca26ed38e1b69971c465f56becfbcac4ed87444
fcarella <fred.carella@gmail.com>
2025-01-05 11:10:50 -0500

    commit

--- src/main/java/csd230/lecture_jpa_practical_example/Application.java
+++ src/main/java/csd230/lecture_jpa_practical_example/Application.java
@@ -1,5 +1,7 @@
 package csd230.lecture_jpa_practical_example;
 
+import com.github.javafaker.Commerce;
+import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -22,10 +24,28 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Product newProduct = new Product("Laptop", "High performance laptop", 1200.00);
-		Product newProduct2 = new Product("Laptop", "Low performance laptop", 1000.00);
+		Faker faker = new Faker();
+		Commerce cm = faker.commerce();
+		com.github.javafaker.Number number = faker.number();
+		com.github.javafaker.Book fakeBook = faker.book();
+		String name=cm.productName();
+		String description=cm.material();
+		Product newProduct = new Product(name, description, number.randomDouble(2,10,100));
+		name=cm.productName();
+		description=cm.material();
+		Product newProduct2 = new Product(name, description, number.randomDouble(2,10,100));
+		name=cm.productName();
+		description=cm.material();
+		Product newProduct3 = new Product(name, description, number.randomDouble(2,10,100));
+		name=cm.productName();
+		description=cm.material();
+		Product newProduct4 = new Product(name, description, number.randomDouble(2,10,100));
+//		Product newProduct = new Product("Laptop", "High performance laptop", 1200.00);
+//		Product newProduct2 = new Product("Laptop", "Low performance laptop", 1000.00);
 		productRepository.save(newProduct);
 		productRepository.save(newProduct2);
+		productRepository.save(newProduct3);
+		productRepository.save(newProduct4);
 
 		List<Product> allProducts = productRepository.findAll();
 
@@ -36,21 +56,22 @@ public class Application implements CommandLineRunner {
 			System.out.println(product);
 		}
 
-		productOptional = Optional.ofNullable(productRepository.findFirstByName("Laptop"));
+//		productOptional = Optional.ofNullable(productRepository.findFirstByName("Laptop"));
+		productOptional = Optional.ofNullable(productRepository.findFirstByName(newProduct.getName()));
 		if(productOptional.isPresent()){
 			Product product = productOptional.get();
 			// Do something with the product.
 			System.out.println(product);
 		}
 
-		List<Product> laptops = productRepository.findAllByName("Laptop");
+		List<Product> laptops = productRepository.findAllByName(newProduct2.getName());
 		laptops.forEach(System.out::println);
 
-		Product newProduct3 = new Product("ssd", "100GB drive", 200.00);
-		productRepository.save(newProduct3);
+//		Product newProduct3 = new Product("ssd", "100GB drive", 200.00);
+//		productRepository.save(newProduct3);
 
-		laptops = productRepository.findAll();
-		laptops.forEach(System.out::println);
+		allProducts = productRepository.findAll();
+		allProducts.forEach(System.out::println);
 
 
 	}
